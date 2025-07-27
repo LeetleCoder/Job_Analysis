9263f433a6eee2f653eeb2ab1d77551667bcb16f
Scott <144505776+LeetleCoder@users.noreply.github.com>
2025-07-27 12:53:00 -0400

    Changing filter to FL

--- sql_project/1_top_jobs.sql
+++ sql_project/1_top_jobs.sql
@@ -18,7 +18,7 @@ FROM
     LEFT JOIN company_dim
         ON job_postings_fact.company_id = company_dim.company_id
 WHERE
-    job_work_from_home -- Checks if job_work_from_home IS TRUE
+    job_location LIKE '%FL%'
     AND salary_year_avg IS NOT NULL
     AND job_title_short = 'Data Analyst'
 ORDER BY
--- sql_project/2_top_job_skills.sql
+++ sql_project/2_top_job_skills.sql
@@ -16,7 +16,7 @@ WITH top_jobs AS (
         LEFT JOIN company_dim
             ON job_postings_fact.company_id = company_dim.company_id
     WHERE
-        job_work_from_home -- Checks if job_work_from_home IS TRUE
+        job_location LIKE '%FL%'
         AND salary_year_avg IS NOT NULL
         AND job_title_short = 'Data Analyst'
     ORDER BY
